3b7c912080cc22af068b58711fc18ab48c16bc86
YoWaitAMinute <yowaitaminuteprofessional@gmail.com>
2024-02-15 14:56:00 -0600

    Update sh_load.lua

--- lua/autorun/sh_load.lua
+++ lua/autorun/sh_load.lua
@@ -1,15 +1,56 @@
-hook.Add("DarkRPFinishedLoading", "FMoney:DarkRPFinishedLoading", function()
-    FRedMoney.IncludeDir("flexredmoney/sh_config.lua")
-    FRedMoney.IncludeDir("flexredmoney/sh_utils.lua")
-    FRedMoney.IncludeDir("flexredmoney/sv_utils.lua")
-end)
 
-print([[
-    ------------------------------------------------
-    |                                              |
-    |       FLEX REDMONEY LOADED SUCCESSFULLY      |
-    |          Created By: YoWaitAMinute           |
-    |               Version: 1.0.0                 |
-    |                                              |
-    ------------------------------------------------
-]])
+function FRedMoney.AddFile(File, dir)
+    local fileSide = string.lower(string.Left(File, 3))
+    if SERVER and fileSide == "sv_" then
+        include(dir..File)
+        print("[FRedMoney] SV INCLUDE: " .. File)
+    elseif fileSide == "sh_" then
+        if SERVER then 
+            AddCSLuaFile(dir..File)
+            print("[FRedMoney] SH ADDCS: " .. File)
+        end
+        include(dir..File)
+        print("[FRedMoney] SH INCLUDE: " .. File)
+    elseif fileSide == "cl_" then
+        if SERVER then 
+            AddCSLuaFile(dir..File)
+            print("[FRedMoney] CL ADDCS: " .. File)
+        else
+            include(dir..File)
+            print("[FRedMoney] CL INCLUDE: " .. File)
+        end
+    end
+end
+
+function FRedMoney.IncludeDir(dir)
+    dir = dir .. "/"
+    local File, Directory = file.Find(dir.."*", "LUA")
+
+    for k, v in ipairs(File) do
+        if string.EndsWith(v, ".lua") then
+            FRedMoney.AddFile(v, dir)
+        end
+    end
+
+    for k, v in ipairs(Directory) do
+        print("[FRedMoney] Directory: " .. v)
+        FRedMoney.IncludeDir(dir..v)
+    end
+end
+
+
+
+hook.Add("DarkRPFinishedLoading", "FMoney:DarkRPFinishedLoading", function()
+    FRedMoney.IncludeDir("flexredmoney")
+    print('FRedMoney : Loaded')
+    print([[
+        ------------------------------------------------
+        |                                              |
+        |       FLEX REDMONEY LOADED SUCCESSFULLY      |
+        |          Created By: YoWaitAMinute           |
+        |               Version: 1.0.0                 |
+        |                                              |
+        ------------------------------------------------
+    ]])
+        
+end)
